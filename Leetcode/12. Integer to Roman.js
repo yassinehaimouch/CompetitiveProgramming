5d0bc35d99e1ad477b786ae7583d8540630e4eff
yassinehaimouch <haymouch53@gmail.com>
2023-03-25 14:38:10 +0000

    Medium

---
+++ Leetcode/12. Integer to Roman.js
@@ -0,0 +1,12 @@
+var intToRoman = function (num) {
+  const numbers = {
+      'M': 1000, 'CM': 900, 'D': 500, 'CD': 400, 'C': 100, 'XC': 90, 'L': 50, 'XL': 40, 'X': 10, 'IX': 9, 'V': 5, 'IV': 4, 'I': 1,
+  }
+  let res = '';
+  Object.entries(numbers).forEach(([k, v]) => {
+      res += k.repeat(Math.floor(num / v));
+      num %= v
+  })
+
+  return res;
+};
