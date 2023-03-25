5e1fc03e0e49171612096f444c4aa1f10465b3c4
yassinehaimouch <haymouch53@gmail.com>
2023-03-25 14:57:09 +0000

    Easy

---
+++ Leetcode/412. Fizz Buzz.js
@@ -0,0 +1,20 @@
+var fizzBuzz = function(number) {
+  const arr = [];
+    for(i = 1; i <= number; i++){
+      
+      if(i % 3 == 0 && i % 5 == 0){
+        arr.push("FizzBuzz");
+      }
+  
+     else if(i % 3 == 0) {
+        arr.push("Fizz");
+      }
+      else if(i % 5 == 0){
+        arr.push("Buzz");
+      }
+      
+      else
+        arr.push(i.toString());
+    }
+    return arr;
+  }
