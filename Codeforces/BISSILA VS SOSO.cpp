564723d30f7cdc042b6d61f3421176994292a36e
yassinehaimouch <haymouch53@gmail.com>
2023-03-27 17:04:27 +0000

    BISSILA VS SOSO

---
+++ Codeforces/BISSILA VS SOSO.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{   int number;
+    cin>>number;
+    if(number%2 == 0){
+        cout<<"BISSILA";
+    } else{
+        cout<<"SOSO";
+    }
+    return 0;
+}
